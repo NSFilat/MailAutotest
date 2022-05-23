9ae9dd174543e89f41d518579c95691b06d30fce
nsfilat <nsfilat>
2022-05-24 01:44:42 +0300

    Add linux-friendly backslash in filePath Maximize browser window

--- src/test/java/seleniumTests/BaseDriverTest.java
+++ src/test/java/seleniumTests/BaseDriverTest.java
@@ -3,7 +3,6 @@ package seleniumTests;
 import org.junit.Rule;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -31,6 +30,7 @@ public class BaseDriverTest {
                     e.printStackTrace();
                 }
                 driver = new RemoteWebDriver(hub, options);
+                driver.manage().window().maximize();
 
                 // Запуск на браузера на втором мониторе
                 // driver.manage().window().setPosition(new Point(3000, 0));
--- src/test/java/seleniumTests/firstTests/ExamTest.java
+++ src/test/java/seleniumTests/firstTests/ExamTest.java
@@ -69,7 +69,8 @@ public class ExamTest extends BaseDriverTest {
         mmpo.assertInMessageThemeText(firstTheme);
         mmpo.assertMessageText(firstMessageText);
         mmpo.moveToViewAttachmentLink();
-        mmpo.assertAttachmentNameText(filePath.substring(filePath.lastIndexOf('\\') + 1));
+        mmpo.assertAttachmentNameText(filePath.substring(filePath.lastIndexOf('\\') + 1).
+                substring(filePath.lastIndexOf('/') + 1));
         mmpo.moveToDownloadAttachmentLink();
         mmpo.clickDownloadAttachmentLink();
 
@@ -79,7 +80,8 @@ public class ExamTest extends BaseDriverTest {
         cpo.clickFromMailButton();
         cpo.clickExpandMessageButton();
         cpo.clickIncomingMessageButton();
-        cpo.assertFileNameText(filePath.substring(filePath.lastIndexOf('\\') + 1, filePath.lastIndexOf('.')));
+        cpo.assertFileNameText(filePath.substring(filePath.lastIndexOf('\\') + 1, filePath.lastIndexOf('.')).
+                substring(filePath.lastIndexOf('/') + 1));
         cpo.closeWindow();
 
         //переход в настройки, установка новой подписи
